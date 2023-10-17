18f26a613db7e9ad5809e14a0c77d38bbd46ae8b
Akmal Mukhsimov <aka.mukhsimov@gmail.com>
2023-10-17 16:10:30 +0500

    test

--- src/airflow_dwh_etl_framework/etl/airflow_etl.py
+++ src/airflow_dwh_etl_framework/etl/airflow_etl.py
@@ -5,6 +5,8 @@ import os
 from airflow.models import Variable
 from .. import SparkConnector
 
+def foo():
+    print(123123123)
 
 class AirflowETL:
     def __init__(self, dag: DAG):
@@ -86,10 +88,6 @@ class AirflowETL:
         #     jdbc_df = spark_conn.read_jdbc(url=url, driver=driver, query=query)
         #     jdbc_df.write.orc(datalake_target_path, mode='overwrite')
 
-    @classmethod
-    def _extract_db_full1(cls, task_id, source_system_name, source_system_tag, scheme, table_name, params=None):
-        print(123123123)
-
     def extract_db(self, source_system_name, source_system_tag, scheme, table_name,
                    mode, params=None) -> BaseOperator:
         """
@@ -109,16 +107,8 @@ class AirflowETL:
 
         task_id = f"task_extract_" \
                   f"{source_system_name}_{source_system_tag}_{table_name}_full"
-        return PythonOperator(python_callable=AirflowETL._extract_db_full1,
+        return PythonOperator(python_callable=foo,
                               task_id=task_id,
-                              op_kwargs={
-                                  "task_id": task_id,
-                                  "source_system_name": source_system_name,
-                                  "source_system_tag": source_system_tag,
-                                  "scheme": scheme,
-                                  "table_name": table_name,
-                                  "params": params,
-                              },
                               dag=self.dag)
 
         if mode == 'full':
