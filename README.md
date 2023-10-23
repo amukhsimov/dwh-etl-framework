e8988161145795ce4a61af8a70e6a48f2078680b
Akmal Mukhsimov <aka.mukhsimov@gmail.com>
2023-10-23 14:24:48 +0500

    docs

--- README.md
+++ README.md
@@ -1,1 +1,40 @@
-Hi there! 
+# ETL Framework
+
+## `AirflowETL` class
+
+### - `extract_db()`
+*Parameters:*
+ - `source_system_name`
+ - `source_system_tag`
+ - `scheme`
+ - `table_name`
+ - `read_mode`
+ - `write_mode`
+
+*This function returns an airflow task which extracts data from source system
+and writes it to datalake (S3) in format of `hudi` in the following path: <br>
+`s3a//datalake/<source_system_name>/<source_system_tag>/<scheme>/<table_name>"`*
+
+**Connection details** must be specified in airflow variables with the following naming:
+`<source_system_tag.upper()>_<source_system_name.upper()>_CONN` <br>
+*Value of this variable should have the following yaml structure:*
+```
+url: jdbc...
+host: ip address
+port: port
+sid: sid for oracle
+dbname: dbname for postgres
+username: database username
+password: user password
+```
+
+`read_mode` - *specifies whether to load full table or to load only incremental data.* <br>
+Possible values:
+ - `full` - _reads full table._
+ - `delta` - _reads only incremental data._
+
+`write_mode` - *specifies whether to truncate target table before appending data.* <br>
+Possible values:
+ - `overwrite` - _truncates target table and writes the data._
+ - `append` - _appends data to the target table without deleting data._
+
--- src/airflow_dwh_etl_framework/etl/airflow_etl.py
+++ src/airflow_dwh_etl_framework/etl/airflow_etl.py
@@ -38,8 +38,18 @@ class ETLUtils:
             cur.close()
 
         @classmethod
-        def load_sql_script_spark(cls, conn_info, sql_script, spark):
-            raise NotImplementedError()
+        def load_sql_script_spark(cls, greenplum_conn, sql_script, spark):
+            host = greenplum_conn['host']
+            port = greenplum_conn['port']
+            dbname = greenplum_conn['dbname']
+
+            return spark.read.format('jdbc') \
+                .option("url", f"jdbc:postgresql://{host}:{port}/{dbname}") \
+                .option("query", sql_script) \
+                .option("user", "spark") \
+                .option("password", "") \
+                .option("driver", "org.postgresql.Driver") \
+                .load()
 
         @classmethod
         def load_sql_script(cls, conn_info, query):
@@ -215,7 +225,46 @@ class ETLUtils:
 
     class Spark:
         @classmethod
-        def execute_sql_file(cls, spark, filename, cache_dir=None, alias=None):
+        def execute_sql_script(cls, spark, sql_script, cache_dir=None,
+                               alias=None, engine='spark', **kwargs):
+            """
+            Runs sql script in a given spark session, caches result and loads it as a view if
+                cache_dir and alias is provided
+            :param spark: spark session
+            :param sql_script: sql script to load
+            :param cache_dir: directory in datalake to cache data
+            :param alias: alias to create temp view in spark catalog
+            :param engine: greenplum/spark
+            :param **kwargs: {'greenplum_conn': 'optional conn info'}
+            :return:
+            """
+            if engine == 'spark':
+                # run sql script via spark session
+                df = spark.sql(sql_script)
+            elif engine == 'greenplum':
+                if 'greenplum_conn' in kwargs:
+                    greenplum_conn = kwargs['greenplum_conn']
+                else:
+                    greenplum_conn = yaml.safe_load(Variable.get('MAIN_GREENPLUM_CONN'))
+
+                df = ETLUtils.Postgres.load_sql_script_spark(
+                    greenplum_conn=greenplum_conn, sql_script=sql_script, spark=spark,
+                )
+            else:
+                raise ValueError(f"Invalid engine: '{engine}'")
+            # if cache_dir is specified - cache it
+            if cache_dir:
+                df.write.orc(cache_dir, mode='overwrite')
+                df = spark.read.orc(cache_dir)
+            # if alias is specified - create a view
+            if alias:
+                df.createOrReplaceTempView(alias)
+            return df
+
+
+        @classmethod
+        def execute_sql_file(cls, spark, filename, cache_dir=None,
+                               alias=None, engine='spark', **kwargs):
             """
             Runs sql file in a given spark session, caches result and loads it as a view if
                 cache_dir and alias is provided
@@ -231,16 +280,12 @@ class ETLUtils:
             # read sql script from file
             with open(filename, 'rt') as fp:
                 sql_script = fp.read()
-            # run sql script via spark session
-            df = spark.sql(sql_script)
-            # if cache_dir is specified - cache it
-            if cache_dir:
-                df.write.orc(cache_dir, mode='overwrite')
-                df = spark.read.orc(cache_dir)
-            # if alias is specified - create a view
-            if alias:
-                df.createOrReplaceTempView(alias)
-            return df
+
+            return ETLUtils.Spark.execute_sql_script(spark=spark,
+                                                     sql_script=sql_script,
+                                                     cache_dir=cache_dir,
+                                                     alias=alias)
+
         @classmethod
         def load_dependencies(cls, dependencies, spark):
             """
@@ -279,7 +324,7 @@ class ETLUtils:
                     raise ValueError(f"load_dependencies_into_spark(): Invalid schema: '{schema}'")
                 if not source_table_name:
                     raise ValueError(f"load_dependencies_into_spark(): Invalid source_table_name: '{source_table_name}'")
-                if not format:
+                if format not in ('jdbc', 'datalake'):
                     raise ValueError(f"load_dependencies_into_spark(): Invalid format: '{format}'")
                 if not alias:
                     raise ValueError(f"load_dependencies_into_spark(): Invalid alias: '{alias}'")
@@ -290,10 +335,47 @@ class ETLUtils:
                         source_system_tag.lower(), schema.lower(), source_table_name.lower()
                     )
                     source_df = spark.read.format(format).load(datalake_path)
+                elif source == 'greenplum':
+                    greenplum_conn = yaml.safe_load(Variable.get('MAIN_GREENPLUM_CONN'))
+                    host = greenplum_conn['host']
+                    port = greenplum_conn['port']
+                    dbname = greenplum_conn['dbname']
+
+                    source_df = spark.read.format(format) \
+                        .option("url", f"jdbc:postgresql://{host}:{port}/{dbname}") \
+                        .option("dbtable", f"{schema}.{source_table_name}") \
+                        .option("user", "spark") \
+                        .option("password", "") \
+                        .option("driver", "org.postgresql.Driver") \
+                        .load()
                 else:
                     raise ValueError(f"Invalid source type: '{source}'")
 
                 source_df.createOrReplaceTempView(alias)
+
+    @classmethod
+    def fill_sql_parameters(cls, sql_script, parameters):
+        """
+        Fills given sql script with parameters specified.
+        :param sql_script: sql script text
+        :param parameters: parameters to fill into sql script. Has the following format:
+            [ {'name': 'param name', 'type': 'param type', 'value': 'only for constant param type'}, ... ]
+            Where type: report_date, constant
+        :return: str
+        """
+        for param in parameters:
+            name = param['name']
+            _type = param['type']
+
+            if _type == 'report_date':
+                report_date = Variable.get('REPORT_DATE')
+                sql_script = sql_script.format(**{name: report_date})
+            elif _type == 'constant':
+                value = param['value']
+                sql_script = sql_script.format(**{name: value})
+
+        return sql_script
+
 
 
 class AirflowETL:
@@ -307,7 +389,8 @@ class AirflowETL:
         :param source_system_tag: Source system tag (e.g.: main, test, prod)
         :param scheme: Scheme name in source database
         :param table_name: Table
-        :param params: load parameters
+        :param write_mode: 'overwrite' - truncates target table and writes the data.
+                           'append' - appends data to the target table without deleting data.
         :return:
         """
         if write_mode not in ('overwrite', 'append'):
@@ -445,9 +528,20 @@ class AirflowETL:
         """
         # check step type
         step_type = transform_step['type']
+        engine = 'spark' if 'engine' not in transform_step else transform_step['engine']
+
+        assert engine in ('spark', 'greenplum')
         assert step_type in ('cache', 'final')
+
         # fetch sql file
-        sql_file = os.path.join(table_folder, transform_step['sql'])
+        with open(os.path.join(table_folder, transform_step['sql']), 'rt') as fp:
+            sql_script = fp.read()
+        # if parameters are specified
+        if 'parameters' in transform_step:
+            params = transform_step['parameters']
+            # format sql script with given parameters
+            sql_script = ETLUtils.fill_sql_parameters(sql_script=sql_script, parameters=params)
+
         if step_type == 'cache':
             if 'alias' not in transform_step:
                 raise ValueError(f"No alias provided for sql step type 'cache'")
@@ -455,17 +549,19 @@ class AirflowETL:
             if not alias or len(alias) <= 1:
                 raise ValueError(f"Too short alias: '{alias}'")
 
-            ETLUtils.Spark.execute_sql_file(
+            ETLUtils.Spark.execute_sql_script(
                 spark=spark,
-                filename=sql_file,
+                sql_script=sql_script,
                 cache_dir=os.path.join(ETLUtils.Datalake.get_dump_dir(), f"{task_id}", alias),
                 alias=alias,
+                engine=engine,
             )
             return None
         elif step_type == 'final':
-            df = ETLUtils.Spark.execute_sql_file(
+            df = ETLUtils.Spark.execute_sql_script(
                 spark=spark,
-                filename=sql_file,
+                sql_script=sql_script,
+                engine=engine,
             )
             return df
 
@@ -502,9 +598,6 @@ class AirflowETL:
         :param merge_mode: full/delta
         :return:
         """
-        if read_mode == 'full' and (write_mode == 'append' or merge_mode == 'delta'):
-            raise ValueError(f"_transform_full(): Invalid combination of read_mode, write_mode and merge_mode: "
-                             f"read_mode=='{read_mode}' write_mode=='{write_mode}' and merge_mode=='{merge_mode}'")
         # get target table folder
         table_folder = os.path.join(Variable.get("AIRFLOW_SQL_FOLDER"),
                                     "transform", target_schema.lower(), target_table_name.lower())
